ecfb3e8e5a8b7e1359e0d7037390776db339ed9e
pangff <pangff@gmail.com>
2014-01-29 14:12:04 +0800

    修改上传路径

--- server/requestHandlers.js
+++ server/requestHandlers.js
@@ -27,8 +27,7 @@ function start(response) {
 function upload(response, request) {
   console.log("Request handler 'upload' was called.");
 
-  var form = new formidable.IncomingForm();
-  form.uploadDir="./temp";
+  var form = new formidable.IncomingForm({uploadDir:"./temp"});
   console.log("about to parse");
   form.parse(request, function(error, fields, files) {
     console.log("parsing done");
